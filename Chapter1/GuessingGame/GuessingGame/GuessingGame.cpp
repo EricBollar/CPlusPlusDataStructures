0132fd7cd4187998577ee3b593f1d358f85a8a93
EricBollar <esbollar@gmail.com>
2019-01-21 15:27:47 -0800

    guessing game is finished

--- Chapter1/GuessingGame/GuessingGame/GuessingGame.cpp
+++ Chapter1/GuessingGame/GuessingGame/GuessingGame.cpp
@@ -1,5 +1,27 @@
 #include <iostream>
+#include <time.h>
 
 int main () {
-	std::cout << "Hello World!" << std::endl;
+	srand(time(NULL));
+	int num = rand() % 101;
+	int playerGuess;
+	bool stop = false;
+
+	while (stop == false) {
+		std::cout << "I have picked a number between 0 and 100! What is your guess:" << std::endl;
+		std::cin >> playerGuess;
+
+		if (playerGuess == num) {
+			std::cout << "Your guess was correct!" << std::endl;
+			std::cout << "The number was: " << num << std::endl;
+			system("PAUSE");
+			stop = true;
+		}
+		else if (playerGuess > num) {
+			std::cout << "Your guess is higher than the number. Guess again!" << std::endl;
+		}
+		else if (playerGuess < num) {
+			std::cout << "Your guess is lower than the number. Guess again!" << std::endl;
+		}
+	}
 }
