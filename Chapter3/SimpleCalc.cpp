adc76cc2bfaf791ca6d5ca4c323bbc9cb2bb44c7
EricBollar <esbollar@gmail.com>
2019-02-25 20:57:31 -0800

    setting up Simple Calculator

---
+++ Chapter3/SimpleCalc.cpp
@@ -0,0 +1,10 @@
+#include <iostream>
+#include <string>
+#include <cmath>
+
+int main() {
+    std::string equation;
+
+    std::cout << "Please enter your equation using only adding, subtracting, multiplying, and dividing (+ - * /):";
+    std::cin.getline >> equation;
+}
