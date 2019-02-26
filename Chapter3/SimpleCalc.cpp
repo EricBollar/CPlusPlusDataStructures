6512f0579d4791f1ebc2092f1662b5cb86a671e8
EricBollar <esbollar@gmail.com>
2019-02-26 13:00:48 -0800

    working on calc

--- Chapter3/SimpleCalc.cpp
+++ Chapter3/SimpleCalc.cpp
@@ -8,12 +8,12 @@ int main() {
     std::cout << "Please enter your equation using only adding, subtracting, multiplying, and dividing (+ - * /):" << std::endl;
     getline(std::cin, equation);
 
-    for (int i = 0; i < equation.length(); i++) {
+    for (int i = 0; i < equation.length(); i++) { //finds all multiplication/division left -> right
         if (equation.at(i) == '*' || equation.at(i) == '/') {
             std::cout << "multiplication/division at" << i;
         }
     }
-    for (int i = 0; i < equation.length(); i++) {
+    for (int i = 0; i < equation.length(); i++) { //finds all addition/subtraction left -> right
         if (equation.at(i) == '+' || equation.at(i) == '-') {
             std::cout << "addition/subtraction at " << i;
         }
