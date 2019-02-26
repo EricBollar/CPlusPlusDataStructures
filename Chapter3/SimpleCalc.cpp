6dc615d6f72fb5887710e13e9d02e7566fd9f2dd
EricBollar <esbollar@gmail.com>
2019-02-25 21:06:15 -0800

    Continuing setting up Simple Calc

--- Chapter3/SimpleCalc.cpp
+++ Chapter3/SimpleCalc.cpp
@@ -5,6 +5,8 @@
 int main() {
     std::string equation;
 
-    std::cout << "Please enter your equation using only adding, subtracting, multiplying, and dividing (+ - * /):";
-    std::cin.getline >> equation;
+    std::cout << "Please enter your equation using only adding, subtracting, multiplying, and dividing (+ - * /):" << std::endl;
+    getline(std::cin, equation);
+
+    std::cout << equation;
 }
