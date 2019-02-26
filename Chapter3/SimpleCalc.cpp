bdceb1e7d4cd2130f1eb5f3386ec933f0eef6be5
EricBollar <esbollar@gmail.com>
2019-02-26 12:59:15 -0800

    started working on simple calculator

--- Chapter3/SimpleCalc.cpp
+++ Chapter3/SimpleCalc.cpp
@@ -8,5 +8,14 @@ int main() {
     std::cout << "Please enter your equation using only adding, subtracting, multiplying, and dividing (+ - * /):" << std::endl;
     getline(std::cin, equation);
 
-    std::cout << equation;
+    for (int i = 0; i < equation.length(); i++) {
+        if (equation.at(i) == '*' || equation.at(i) == '/') {
+            std::cout << "multiplication/division at" << i;
+        }
+    }
+    for (int i = 0; i < equation.length(); i++) {
+        if (equation.at(i) == '+' || equation.at(i) == '-') {
+            std::cout << "addition/subtraction at " << i;
+        }
+    }
 }
