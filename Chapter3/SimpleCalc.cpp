5369f5c6c416da0f72f218ea66b6c6013289d296
EricBollar <esbollar@gmail.com>
2019-02-28 21:08:03 -0800

    continued working on simplecalc

--- Chapter3/SimpleCalc.cpp
+++ Chapter3/SimpleCalc.cpp
@@ -1,21 +1,64 @@
 #include <iostream>
 #include <string>
 #include <cmath>
+#include <vector>
+
+const std::string digits = "0123456789"; // 0-9
+
+bool isDigit(char c) {
+    for (int i = 0; i < digits.length(); i++) {
+        if (c == digits.at(i)) {
+            return true;
+        }
+    }
+    return false;
+}
 
 int main() {
-    std::string equation;
+    std::string equation; // variables
+    std::vector<int> numbers; // all integers in equation, in order
+    std::vector<char> symbols; // all symbols (+-*/) in equation, in order
+    int solution; // solution that will be outputed
 
+    //receive equation input
     std::cout << "Please enter your equation using only adding, subtracting, multiplying, and dividing (+ - * /):" << std::endl;
     getline(std::cin, equation);
 
-    for (int i = 0; i < equation.length(); i++) { //finds all multiplication/division left -> right
-        if (equation.at(i) == '*' || equation.at(i) == '/') {
-            std::cout << "multiplication/division at" << i;
+    // fills up symbols with all the symbols of equation in order, left -> right
+    for (int i = 0; i < equation.length(); i++) { //finds all multiplication/division/addition/subtraction symbols left -> right
+        if (equation.at(i) == '*') {
+            symbols.push_back('*');
+            std::cout << '*' << std::endl;
+        }
+        if (equation.at(i) == '/') {
+            symbols.push_back('/');
+            std::cout << '/' << std::endl;
+        }
+        if (equation.at(i) == '+') {
+            symbols.push_back('+');
+            std::cout << '+' << std::endl;
+        }
+        if (equation.at(i) == '-') {
+            symbols.push_back('-');
+            std::cout << '-' << std::endl;
         }
     }
-    for (int i = 0; i < equation.length(); i++) { //finds all addition/subtraction left -> right
-        if (equation.at(i) == '+' || equation.at(i) == '-') {
-            std::cout << "addition/subtraction at " << i;
+ 
+    std::string currNum = ""; // this for loop adds all of the numbers into the numbers vector in order from left -> right
+    for (int i = 0; i < equation.length(); i++) {
+        if (isDigit(equation.at(i))) {
+            currNum += equation.at(i);
+            if (i == equation.length() - 1) {
+                std::cout << currNum << std::endl;
+                numbers.push_back(atoi(currNum.c_str()));
+                currNum = "";
+            }
+        } else if (currNum != "") {
+            std::cout << currNum << std::endl;
+            numbers.push_back(atoi(currNum.c_str()));
+            currNum = "";
+        } else {
+            currNum = "";
         }
     }
 }
